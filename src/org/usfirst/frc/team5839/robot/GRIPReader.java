3151fd5196ed44323c12ccd0fa64ef26f6b7d75c
Zhibo Chen <a1s1d1f1j1k1l1@126.com>
2016-02-15 16:58:17 +0800

    Template Updated

--- src/org/usfirst/frc/team5839/robot/GRIPReader.java
+++ src/org/usfirst/frc/team5839/robot/GRIPReader.java
@@ -1,9 +1,19 @@
 package org.usfirst.frc.team5839.robot;
 
+import java.util.ArrayList;
+
 public class GRIPReader {
 	
 	public static Countours getCountours(){
-		return null;
+		Countours countours = new Countours(new ArrayList<Countour>());
+		
+		//use  
+		/*
+		for (int i = 0; i < countours.size(); i++) {
+			countours.add(new Countour(solidity[i], height[i], centerX[i], centerY[i], area[i], width[i]));
+		}
+		*/
+		return countours;
 	}
 	
 	public static  Lines getLines(){
